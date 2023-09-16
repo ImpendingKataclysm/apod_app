6ba59c13a575225db977e5a4c95ba7b59a7b82cc
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-16 15:29:57 -0700

    Allow user to input location instead of attempting to read user location automatically

--- pages/Moon_Phases.py
+++ pages/Moon_Phases.py
@@ -4,30 +4,35 @@ import streamlit as st
 import geocoder
 
 api_key = st.secrets['WEATHER_API_KEY']
-g = geocoder.ipinfo('me')
-city = g.city
-date = datetime.datetime.now().date()
+user_location = geocoder.ipinfo('me')
+city = st.text_input('Enter your city')
+region = st.text_input('Enter region (province, state, etc)')
 
-request_url = "https://api.weatherapi.com/v1/astronomy.json?" \
-              f"key={api_key}&q={city}&dt={date}"
-response = requests.get(request_url)
-content = response.json()
+if city and region:
+    date = datetime.datetime.now().date()
 
-location = content['location']
-city = location['name']
-region = location['region']
-country = location['country']
-current_date = date.strftime('%B %d, %Y')
+    request_url = "https://api.weatherapi.com/v1/astronomy.json?" \
+                  f"key={api_key}&q={city},{region}&dt={date}"
+    response = requests.get(request_url)
+    content = response.json()
 
-astronomy = content['astronomy']['astro']
-moon_phase = astronomy['moon_phase']
+    location = content['location']
+    city = location['name']
+    region = location['region']
+    country = location['country']
+    current_date = date.strftime('%B %d, %Y')
 
-img_path_components = moon_phase.lower().split()
-img_path = '-'.join(img_path_components)
+    astronomy = content['astronomy']['astro']
+    moon_phase = astronomy['moon_phase']
 
-st.title('Moon Phase')
-st.subheader(f'{city}, {region}, {country}')
-st.write(f'{current_date}')
-st.subheader(moon_phase)
+    img_path_components = moon_phase.lower().split()
+    img_path = '-'.join(img_path_components)
 
-st.image(f'images/{img_path}.png')
+    st.title('Moon Phase')
+    st.subheader(f'{city}, {region}, {country}')
+    st.write(f'{current_date}')
+    st.subheader(moon_phase)
+
+    st.image(f'images/{img_path}.png')
+else:
+    st.write('Unable to process location')
