2b221bd024e5e70070902285ef944997e6066641
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-16 13:52:49 -0700

    Add a new page to the app

--- pages/New.py
+++ pages/New.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import geocoder
 
 api_key = st.secrets['WEATHER_API_KEY']
+location = geocoder.ipinfo('me')
 
 st.title('New Page')
-st.write(api_key)
+st.write(location.latlng)
